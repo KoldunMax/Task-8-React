ddbfeab42c25305333cb218c8d4f2cac3bd76263
Maksym Koldun <koldun.maks@gmail.com>
2018-07-23 15:40:02 +0300

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const increment = () => ({
+  type: "INCREMENT"
+});
+
+const decrement = () => ({
+  type: "DECREMENT"
+})
+
+const Button = ({ onClick, value }) => <button onClick={onClick} type='button'>{value}</button>;
+
+function counter(state = 0, action) {
+    switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+    }
+  }
+
+const logger = store => next => action => {
+    let result = next(action);
+    console.log("next state", store.getState());
+    return result; 
+}
+  
+let store = createStore(counter, applyMiddleware(logger));
+
+const render = () => {
+    ReactDOM.render(
+      <div>
+          <p>{store.getState()}</p>
+          <Button onClick={() => store.dispatch(increment())} value={'+'} />
+          <Button onClick={() => store.dispatch(decrement())} value={'-'} />
+      </div>
+      ,document.getElementById('root')
+    )
+}
+
+store.subscribe(render);
+
+render();
